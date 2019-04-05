ec88d46329a356ea5e78c77cd39bd6c74574c139
Chris Lebon <chris@chris.me>
2019-04-05 17:28:58 +0200

    fixed weather link

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.get('/help', (req, res) => {
     });
 });
 
-app.get('/weather', (req, res) => {
+app.get('/weather/*', (req, res) => {
     if (!req.query.address) {
         return res.send({
             error: 'Please provide an address'
